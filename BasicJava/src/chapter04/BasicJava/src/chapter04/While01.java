5874e200b44d6e44e271f9fd17b151730c90c2e6
Koj.__ <58206066+KoJaehun@users.noreply.github.com>
2019-11-26 16:44:21 +0900

    자바 챕터4
    Do it 자바 프로그래밍 챕터4의 제어문
     - IfElse
     - While
     - 학점계산기(if,while)

---
+++ BasicJava/src/chapter04/BasicJava/src/chapter04/While01.java
@@ -0,0 +1,29 @@
+package chapter04;
+
+public class While01 {
+
+	public static void main(String[] args) {
+		
+		//횟수가 정해져있지 않은 반복문
+		
+		// while(조건문) {
+		//		조건이 참인 경우 반복적으로 실행되는 코드
+		// }
+		
+		// 무한루프
+		//		while(true) { 
+		//system.out.println("참인경우 반복");
+		//		}
+
+		int num = 1;
+		while(true) {
+			System.out.println(num);
+			num = num+1;
+			if(num > 10) {
+				break;
+			}
+		}
+
+	}
+}
+
